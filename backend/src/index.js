f406407cdd28b3fac7150dae333ed022d7878514
Muhammad Taha <tahaaa1029@gmail.com>
2026-01-13 17:02:38 +0500

    Project completed

---
+++ backend/src/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cookieparser from "cookie-parser";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+import path from "path";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieparser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+    console.log("server is running on port:" +PORT);
+    connectDB()
+});
